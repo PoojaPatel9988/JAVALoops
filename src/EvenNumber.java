817cb7df7326cd7c752ebf0055d865a4a65614a2
Pooja Patel <studykhuswaha01@gmail.com>
2025-12-25 21:26:32 +0530

    Check EvenNumber using a whileLoop

---
+++ src/EvenNumber.java
@@ -0,0 +1,13 @@
+public class EvenNumber {
+    public static void main(String[] args) {
+        int num=1;
+        int count =0;
+        while(count < 10){
+            if( num % 2 == 0){
+                System.out.println(num);
+                count ++;
+            }
+            num ++;
+        }
+    }
+}
