0ee20dcbd3f68af7a0f034de43a8114da7096c5a
Pooja Patel <studykhuswaha01@gmail.com>
2025-12-25 21:23:17 +0530

    check divisibility using a While loop

---
+++ src/Divisibility.java
@@ -0,0 +1,11 @@
+public class Divisibility {
+    public static void main(String[] args) {
+        int num =1;
+        do {
+            if (num % 3 == 0 && num % 5 == 0) {
+                System.out.println(num);
+            }
+            num++;
+        }while(num <= 100);
+    }
+}
