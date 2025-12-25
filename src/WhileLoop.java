594fed3546ee266f6fe0db825d022f240d02038d
Pooja Patel <studykhuswaha01@gmail.com>
2025-12-25 21:53:07 +0530

    add WhileLoop class

---
+++ src/WhileLoop.java
@@ -0,0 +1,17 @@
+/*
+print any message in sequence of 1 to 10.
+ */
+public class WhileLoop
+{
+    public static void main(String[] args)
+    {
+        int number;   //declaretion
+        number = 1;   //initilization
+        while(1 <= 10)  //condition
+        {
+            System.out.println(number + ".hey there!");
+            number ++;
+        }
+
+    }
+}
