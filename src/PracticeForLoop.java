f35b59c2b63bfbf57988ca040ff8f1943e689d3f
Pooja Patel <studykhuswaha01@gmail.com>
2025-12-25 21:37:33 +0530

    add practice file using for loop

---
+++ src/PracticeForLoop.java
@@ -0,0 +1,35 @@
+public class PracticeForLoop
+{
+    public static void main(String[] args) {
+/*
+//PRINT HII 5 TIMES
+    for (int i=1;i<=5;i++)
+          {
+              System.out.println(i + " Hii...");
+          }
+*/
+/*
+        //PRINT HII reverse order 100 to 1 TIMES
+        for(int s=100;s>=1;s--){
+            System.out.println(s + " hii");
+        }
+*/
+
+
+/*
+  //INFINITE LOOPS
+        for(int i=0; ; i++){
+            System.out.println("hello");
+        }
+*/
+
+        //PRINT COUNTING 1 T0 10.
+        for(int i=0;i <11;i++){
+            System.out.println(i);
+        }
+
+        for(int count=0; count <11;count++){
+            System.out.println(count + " ");
+        }
+    }
+}
