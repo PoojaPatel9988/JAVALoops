fa7c355f648233e95629b13d420f72cdd79ef49c
Pooja Patel <studykhuswaha01@gmail.com>
2025-12-25 21:39:39 +0530

    add PracticeWhileLoop class

---
+++ src/PracticeWhileLoop.java
@@ -0,0 +1,10 @@
+public class PracticeWhileLoop {
+    public static void main(String[] args) {
+        //PRINT NUMBERS 0 TO 10
+        int number=0;
+        while(number < 10){
+            System.out.println(number);
+            number = number + 1;
+        }
+    }
+}
