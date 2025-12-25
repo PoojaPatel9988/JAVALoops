dccb2d516d5c76531647a8c7bbfd27bb28ffe8db
Pooja Patel <studykhuswaha01@gmail.com>
2025-12-25 21:34:24 +0530

    Add PracticeDoWhileLoop class

---
+++ src/PracticeDoWhileLoop.java
@@ -0,0 +1,10 @@
+public class PracticeDoWhileLoop {
+    public static void main(String[] args) {
+        //PRINT COUNT 1 TO 10.
+        int i =0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i < 10);
+    }
+}
